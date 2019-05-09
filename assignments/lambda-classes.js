746f97e9495314f331539ae1f74b6b49c803d545
steven122185 <49796406+steven122185@users.noreply.github.com>
2019-05-09 16:58:16 -0400

    Completed the assignment -stretch items

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -6,7 +6,7 @@ class Person {
         this.location = personAttributes.location;
     }
     speak() {
-        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
     }
 }
 
@@ -32,15 +32,16 @@ class Student extends Person {
         this.className = studentAttributes.className;
         this.favSubjects = studentAttributes.favSubjects;
     }
-    listsSubjects() {
-        this.favSubjects.foreach(function(element) {
-            console.log(element);
-        });
+    listsSubjects(favSubjects, length) {
+        for (let i = 0; i < length; i++) {
+            console.log(favSubjects[i]);
+        }
     }
+
     PRAssignment(subject) {
         console.log(`${this.name} has submitted a PR for ${subject}`);
     }
-    sprintChallenge() {
+    sprintChallenge(subject) {
         console.log(`${this.name} has begun sprint challenge on ${subject}`);
     }
 }
@@ -60,9 +61,92 @@ class ProjectManager extends Instructor {
 }
 
 // Students -------------------------------
+const firstStudent = {
+    name: 'Steven',
+    age: 33,
+    location: 'Deland',
+    previousBackground: 'Sorcerer to the king',
+    className: 'Javascript 4',
+    favSubjects: ['Javascript', 'CSS', 'HTML']
+}
+const secondStudent = {
+    name: 'Tyler',
+    age: 18,
+    location: 'Miami',
+    previousBackground: 'Court Jester',
+    className: 'Javascript 4',
+    favSubjects: ['Lunch', 'Javascript', 'CSS', 'HTML']
+}
+const thirdStudent = {
+    name: 'Sarah',
+    age: 21,
+    location: 'North Pole',
+    previousBackground: 'Santas Elf',
+    className: 'Javascript 4',
+    favSubjects: ['Lunch', 'Javascript', 'CSS', 'HTML', 'Making toys']
+}
 
+const steven = new Student(firstStudent);
+const tyler = new Student(secondStudent);
+const sarah = new Student(thirdStudent);
 
 // Instructors ----------------------------
-
+const firstInstructor = {
+    name: 'David',
+    age: 38,
+    location: 'Westeros',
+    specialty: 'Back-end',
+    favLanguage: 'Java-script',
+    catchPhrase: 'What is dead may never die'
+}
+const secondInstructor = {
+    name: 'Jean-Luc',
+    age: 26,
+    location: 'Enterprise NCC-1701',
+    specialty: 'Back-end',
+    favLanguage: 'Java-script',
+    catchPhrase: 'Make it so #1'
+}
+const david = new Instructor(firstInstructor);
+const frank = new Instructor(secondInstructor);
 
 // Project Managers -----------------------
+const firstProjectManager = {
+    name: 'Diane',
+    age: 42,
+    location: 'Chicago',
+    specialty: 'Back-end',
+    favLanguage: 'Java-script',
+    catchPhrase: 'Winter is Here!',
+    gradClassName: 'Web18',
+    favInstructor: 'Jean-Luc'
+}
+const secondProjectManager = {
+    name: 'Jawarhula',
+    age: 30,
+    location: 'Midgard',
+    specialty: 'Front-End',
+    favLanguage: 'Java-script',
+    catchPhrase: 'Winter is Here!',
+    gradClassName: 'Web18',
+    favInstructor: 'David'
+}
+
+const diane = new ProjectManager(firstProjectManager);
+const Jawarhula = new ProjectManager(secondProjectManager);
+
+// Some testing!
+steven.listsSubjects(steven.favSubjects, steven.favSubjects.length);
+tyler.PRAssignment('biology')
+sarah.sprintChallenge('Battle-tech');
+steven.speak();
+
+david.demo(tyler.favSubjects[0]);
+frank.grade(sarah, sarah.favSubjects[3]);
+david.speak();
+
+diane.standUp('web20_adam');
+Jawarhula.grade(firstStudent, 'voyeurism');
+Jawarhula.speak();
+
+
