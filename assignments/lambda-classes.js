e6402444bc7f9e2dfe0b46fbf34e59b73415b939
steven122185 <49796406+steven122185@users.noreply.github.com>
2019-05-09 13:30:42 -0400

    Completed refactoring yesterdays code

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,20 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(personAttributes) {
+        this.name = personAttributes.name;
+        this.age = personAttributes.age;
+        this.location = personAttributes.location;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    }
+}
+
+class Instructor extends Person {
+    constructor(instructorAttributes) {
+        super(instructorAttributes);
+        this.specialty = instructorAttributes.specialty;
+        this.favLanguage = instructorAttributes.favLanguage;
+        this.catchPhrase = instructorAttributes.catchPhrase;        
+    }
+}
