1301ddd9d480e0958771191abac729d0635724ae
steven122185 <49796406+steven122185@users.noreply.github.com>
2019-05-09 16:00:54 -0400

    Created the classes for the Lambda school

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -17,4 +17,52 @@ class Instructor extends Person {
         this.favLanguage = instructorAttributes.favLanguage;
         this.catchPhrase = instructorAttributes.catchPhrase;        
     }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+    grade (student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
 }
+
+class Student extends Person {
+    constructor(studentAttributes) {
+        super(studentAttributes);
+        this.previousBackground = studentAttributes.previousBackground;
+        this.className = studentAttributes.className;
+        this.favSubjects = studentAttributes.favSubjects;
+    }
+    listsSubjects() {
+        this.favSubjects.foreach(function(element) {
+            console.log(element);
+        });
+    }
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}`);
+    }
+    sprintChallenge() {
+        console.log(`${this.name} has begun sprint challenge on ${subject}`);
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor(pmAttributes) {
+        super(pmAttributes);
+        this.gradClassName = pmAttributes.gradClassName;
+        this.favInstructor = pmAttributes.favInstructor;
+    }
+    standUp(slackChannel) {
+        console.log(`${this.name} announces to ${slackChannel}, @${slackChannel} standy times!`);
+    }
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+    }
+}
+
+// Students -------------------------------
+
+
+// Instructors ----------------------------
+
+
+// Project Managers -----------------------
